a833ec02779ee40f2f2188668d25e5aefdafdf4b
Buğra Çayır <bugra-net@hotmail.com>
2023-07-23 01:12:08 +0300

    !! in this version tasks are started when the api endpoints called !!
    this is another version and not the primary aim of the service. for instance, if you want to start usom task, go to http://localhost:8000/usom and then go to 
    http://localhost:8000/phishtank and both usom and phishtank functions are working in an asynch manner.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
 from tasks import usom, phishtank, openphish, phishstats
 from celery_base import app
+from fastapi import FastAPI
+
+app_fastapi = FastAPI()  # Create a new FastAPI app
+
+# Define FastAPI endpoints for each task
+@app_fastapi.get("/usom")
+async def run_usom():
+    usom.delay()
+    return {"message": "USOM task has been started"}
+
+@app_fastapi.get("/phishtank")
+async def run_phishtank():
+    phishtank.delay()
+    return {"message": "Phishtank task has been started"}
+
+@app_fastapi.get("/openphish")
+async def run_openphish():
+    openphish.delay()
+    return {"message": "Openphish task has been started"}
+
+@app_fastapi.get("/phishstats")
+async def run_phishstats():
+    phishstats.delay()
+    return {"message": "Phishstats task has been started"}
 
 if __name__ == "__main__":
-    phishtank.delay()
-
-    usom.delay()
-
-    openphish.delay()
-    
-    phishstats.delay()
+    # Start the FastAPI server with Uvicorn
+    import uvicorn
+    uvicorn.run(app_fastapi, host="0.0.0.0", port=8000)
